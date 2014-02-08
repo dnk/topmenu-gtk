dfade684171c59f24ce3332703178fc0d73631fa
Javier <dev.git@javispedro.com>
2014-02-09 00:18:48 +0100

    properly prefix X11 atoms with an underscore

--- global.h
+++ global.h
@@ -6,10 +6,10 @@
 #include "config.h"
 
 /** The window ID of this top level's window attached menu window. */
-#define ATOM_TOPMENU_WINDOW "TOPMENU_WINDOW"
+#define ATOM_TOPMENU_WINDOW "_TOPMENU_WINDOW"
 
 /** The X11 selection that is used to indicate the current server widget. */
-#define ATOM_TOPMENU_SERVER_SELECTION "TOPMENU_SERVER"
+#define ATOM_TOPMENU_SERVER_SELECTION "_TOPMENU_SERVER"
 
 /* Gobject data keys */
 #define OBJECT_DATA_KEY_PLUG "topmenu-plug"
